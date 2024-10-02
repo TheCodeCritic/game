8099783bbf57dfed999fe83bf46eb1a38eaa06e7
TheCodeCritic <132786120+TheCodeCritic@users.noreply.github.com>
2024-10-02 07:56:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-z = 4
-print(z)
+# Guess the number
+import random
+
+num = random.randint(1,999)
+numGuessed = False
+
+while numGuessed == False:
+    guess = int(input("Guess the number: ")
+    if guess > num:
+        print("Lower")
+    elif guess < num:
+        print("Higher")
+    elif guess == num:
+        numGuessed = True
+
+print(f"Correct! The answer is {num}!")
