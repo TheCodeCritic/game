917a691b0f0a425c7a4c28bfb9530750a520912a
TheCodeCritic <132786120+TheCodeCritic@users.noreply.github.com>
2024-10-10 19:27:53 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -9,16 +9,19 @@ print(f"Your card is {str(currentCard)}")
 
 while cards != 21 or cards < 21:
     # main game loop
+    newCard = randint(1, 10)
     choice = input("Hit or fold: ").upper()
+
     if choice == "HIT":
-        newCard = randint(1, 10)
+        if newCard + cards > 21:
+            print(f"You went bust!")
+            print(f"You had {str(cards)}")
+            break
+
         cards = cards + newCard
         print(f"Your card is {str(newCard)}")
     elif choice == "FOLD":
         continue
 
-if cards > 21:
-    print(f"You went bust!")
-    print(f"You had {str(cards)}")
-elif cards == 21:
+if cards == 21:
     print("BLACKJACK!!")
